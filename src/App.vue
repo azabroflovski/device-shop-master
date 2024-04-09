7e9b5ddf2aee1238a8fe1cfba23395ad093a0c75
azabroflovski <azabroflovski@gmail.com>
2024-04-09 22:37:57 +0500

    feat: add simple app layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+
 </script>
 
 <template>
-  <div>
-    <RouterView />
+  <div class="container">
+    <APageHeader title="Amazon">
+      <template #extra>
+        <AInputSearch placeholder="Find something" />
+        <AButton>Create</AButton>
+      </template>
+    </APageHeader>
+
+    <ADivider style="font-size: 12px;">&copy; azabroflovski</ADivider>
   </div>
 </template>
+
+<style>
+.container {
+  max-width: 680px;
+  margin-inline: auto;
+}
+</style>
