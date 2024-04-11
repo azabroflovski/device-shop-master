adf3c180818c638f561c8e803b0063a369db66db
azabroflovski <azabroflovski@gmail.com>
2024-04-12 00:16:52 +0500

    feat: handling 404 routes

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<template>
+  <AResult status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+    <template #extra>
+      <AButton type="primary" @click="$router.push({ name: 'home' })">Back Home</AButton>
+    </template>
+  </AResult>
+</template>
