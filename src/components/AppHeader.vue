444359c949d4ab19233b61ad86f7a48a269c5fe3
azabroflovski <azabroflovski@gmail.com>
2024-04-11 23:01:48 +0500

    feat(AppHeader): go home on logo click

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
 import ProductSearchbar from '@/components/ProductSearchbar.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goHome() {
+  router.push({
+    name: 'home'
+  })
+}
 </script>
 
 <template>
-  <APageHeader title="Device Shop">
+  <APageHeader>
+    <template #title>
+      <span @click="goHome">Device shop</span>
+    </template>
     <template #extra>
       <ProductSearchbar />
       <AButton>Login</AButton>
