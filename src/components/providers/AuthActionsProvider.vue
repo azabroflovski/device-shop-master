3875e95e36aa838d6ae2a64b63b810974b2d267e
azabroflovski <azabroflovski@gmail.com>
2024-04-14 00:25:29 +0500

    fix(AuthActionsProvider): hide from guest & logout handler

--- src/components/providers/AuthActionsProvider.vue
+++ src/components/providers/AuthActionsProvider.vue
@@ -1,19 +1,24 @@
 <script lang="ts" setup>
 import { LogoutOutlined } from '@ant-design/icons-vue'
+import { useAuth } from '@/composables/useAuth'
+
+const { isLoggedIn, logoutWithRedirect } = useAuth()
 </script>
 
 <template>
-  <AFloatButtonGroup>
-    <AFloatButton tooltip="Create product">
-      <template #icon>
-        +
-      </template>
-    </AFloatButton>
+  <div v-if="isLoggedIn">
+    <AFloatButtonGroup>
+      <AFloatButton tooltip="Create product">
+        <template #icon>
+          +
+        </template>
+      </AFloatButton>
 
-    <AFloatButton tooltip="Logout">
-      <template #icon>
-        <LogoutOutlined />
-      </template>
-    </AFloatButton>
-  </AFloatButtonGroup>
+      <AFloatButton @click="logoutWithRedirect" tooltip="Logout">
+        <template #icon>
+          <LogoutOutlined />
+        </template>
+      </AFloatButton>
+    </AFloatButtonGroup>
+  </div>
 </template>
