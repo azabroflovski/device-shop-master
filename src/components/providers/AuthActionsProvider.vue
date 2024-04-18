ee5d9cf0fcf6b5ddc13e1cf81e4110cf61db422c
azabroflovski <azabroflovski@gmail.com>
2024-04-18 23:07:41 +0500

    fix(AuthActionsProvider): handle create product button

--- src/components/providers/AuthActionsProvider.vue
+++ src/components/providers/AuthActionsProvider.vue
@@ -1,14 +1,17 @@
 <script lang="ts" setup>
 import { LogoutOutlined } from '@ant-design/icons-vue'
+import { ref } from 'vue'
+import CreateProductDialog from '@/components/CreateProductDialog.vue'
 import { useAuth } from '@/composables/useAuth'
 
 const { isLoggedIn, logoutWithRedirect } = useAuth()
+const productDialog = ref()
 </script>
 
 <template>
   <div v-if="isLoggedIn">
     <AFloatButtonGroup>
-      <AFloatButton tooltip="Create product">
+      <AFloatButton tooltip="Create product" @click="productDialog.open()">
         <template #icon>
           +
         </template>
@@ -20,5 +23,7 @@ const { isLoggedIn, logoutWithRedirect } = useAuth()
         </template>
       </AFloatButton>
     </AFloatButtonGroup>
+
+    <CreateProductDialog ref="productDialog" />
   </div>
 </template>
