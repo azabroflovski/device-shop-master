f5867dedd71e5ecebc61bea31c0ec76061d8b1bb
azabroflovski <azabroflovski@gmail.com>
2024-04-19 16:35:14 +0500

    chore(useProductsApi): add jsdoc hints

--- src/composables/useProductsApi.ts
+++ src/composables/useProductsApi.ts
@@ -10,6 +10,12 @@ interface UseProductsApiOptions {
   }>
 }
 
+/**
+ * Composable for managing products API data.
+ *
+ * @param {UseProductsApiOptions} [options] - Options for products API.
+ * @returns {object} Object containing products API data and methods.
+ */
 export function useProductsApi(options?: UseProductsApiOptions) {
   const pagination = reactive({
     page: 1,
@@ -24,12 +30,18 @@ export function useProductsApi(options?: UseProductsApiOptions) {
 
   const searchQuery = ref('')
 
+  /**
+   * Indicates whether there are active filters.
+   */
   const hasFilters = computed(() => {
     return filters.status.length || filters.category.length
   })
 
   const { sort, sortingOptions, sortingQueryParams } = useProductsSorting()
 
+  /**
+   * Computed query parameters for the API request.
+   */
   const queryParams = computed(() => {
     let params = {
       _page: pagination.page,
@@ -65,41 +77,60 @@ export function useProductsApi(options?: UseProductsApiOptions) {
     params: queryParams.value,
   }), null, { resetOnExecute: false })
 
+  /**
+   * Computed data from the API response.
+   */
   const data = computed(() => {
     return state.value?.data
   })
 
+  /**
+   * Indicates whether data is loading and there is data available.
+   */
   const isLoadingWithData = computed(() => {
     return isLoading.value && data.value?.length
   })
 
+  /**
+   * Indicates whether data is loading and there is no data available.
+   */
   const isLoadingWithoutData = computed(() => {
     return isLoading.value && !data.value
   })
 
+  /**
+   * Indicates whether there is no data available and loading is complete.
+   */
   const isEmpty = computed(() => {
     return !isLoading.value && !data.value?.length
   })
 
+  /**
+   * Resets filters to default values.
+   */
   function resetFilter() {
     filters.category = []
     filters.status = []
   }
 
+  // Watch for changes in filters and sorting, then execute API request.
   watch([filters, sort], async () => {
     await execute()
   })
 
+  // Watch for changes in filters and reset pagination when filters change.
   watch(filters, () => {
     pagination.page = 1
   })
 
+  // Watch for changes in search query, debounce, and execute API request.
   watch(searchQuery, useDebounceFn(async () => {
     pagination.page = 1
 
     await execute()
   }, 300))
 
+  // Watch for changes in response headers to update pagination total.
   // T_T sorry for any (времени нету)
   watch(() => state.value?.headers, async (headers: any) => {
     if (headers && headers.has('x-total-count'))
