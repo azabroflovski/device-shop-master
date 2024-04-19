79cdaa7c128ebe6db3dd10e4193baad0898fa0b4
azabroflovski <azabroflovski@gmail.com>
2024-04-19 16:39:18 +0500

    chore(useProductsSorting): add jsdoc hints

--- src/composables/useProductsSorting.ts
+++ src/composables/useProductsSorting.ts
@@ -1,9 +1,21 @@
 import { computed, ref } from 'vue'
 import { sortingConfig } from '@/config/product'
 
+/**
+ * Composable function for managing product sorting.
+ * @returns Product sorting composable object.
+ */
 export function useProductsSorting() {
+  // Reference to the currently selected sorting option.
   const sort = ref(sortingConfig.default)
+
+  // Available sorting options.
   const sortingOptions = sortingConfig.options
+
+  /**
+   * Computed property to generate sorting query parameters based on the selected sorting option.
+   * @returns Object containing sorting query parameters.
+   */
   const sortingQueryParams = computed(() => {
     return {
       _sort: sort.value,
