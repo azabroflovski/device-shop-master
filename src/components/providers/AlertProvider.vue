d314c7663f033542f6a779601e442e01fc5307db
azabroflovski <azabroflovski@gmail.com>
2024-04-19 16:45:56 +0500

    chore: alert provider jsdoc hints

--- src/components/providers/AlertProvider.vue
+++ src/components/providers/AlertProvider.vue
@@ -2,6 +2,11 @@
 import { notification } from 'ant-design-vue'
 import { alertBus } from '@/shared/alert-bus'
 
+/**
+ * Handler for the 'REQUEST_LIMIT_ERROR' event.
+ * Opens a notification with the provided title and description.
+ * @param data - Data containing the title and description of the error.
+ */
 alertBus.on('REQUEST_LIMIT_ERROR', ({ title, description }) => {
   notification.open({
     message: title,
--- src/shared/alert-bus.ts
+++ src/shared/alert-bus.ts
@@ -1,6 +1,18 @@
 import mitt from 'mitt'
 
+/**
+ * Event bus for handling alerts.
+ */
 export const alertBus = mitt<{
+  /**
+   * Event emitted when there's a request limit error.
+   * @param data - Data related to the request limit error.
+   */
   REQUEST_LIMIT_ERROR: any
+
+  /**
+   * Event emitted when there's a network error.
+   * @param data - Data related to the network error.
+   */
   NETWORK_ERROR: any
 }>()
